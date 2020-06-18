6f0c2154b201a17bdd160bc0cac69d93a04c5747
Mauricio Gutierrez <developer.mauricio2310@gmail.com>
2020-06-18 09:08:42 -0500

    ajustes

--- resources/views/emails/themes-clients/new-client-register.blade.php
+++ resources/views/emails/themes-clients/new-client-register.blade.php
@@ -1,5 +1,5 @@
 @component('mail::message')
 <h1 style="text-align: center;">Hola {{ $name }} , gracias por suscribirte a {{config('app.name')}}</h1>
-<img class="img-responsive" src="{{ url('/img/logo_lalabag.png.png') }}" style="padding:15%">
+<img class="img-responsive" src="{{ url('/img/logo_lalabag.png') }}" style="padding:15%">
 <p style="text-align: center;">Te notificaremos cuando estemos listos.</p>
 @endcomponent
